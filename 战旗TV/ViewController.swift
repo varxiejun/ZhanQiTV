8abf87aaaaa2b77f0e4023a7bce6ad395820173b
看楼听雨 <273794982@qq.com>
2016-04-14 18:03:54 +0800

    Initial Commit

---
+++ 战旗TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  战旗TV
+//
+//  Created by 看楼听雨 on 16/4/14.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 战旗TVTests/__TVTests.swift
@@ -0,0 +1,36 @@
+//
+//  __TVTests.swift
+//  战旗TVTests
+//
+//  Created by 看楼听雨 on 16/4/14.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+import XCTest
+@testable import __TV
+
+class __TVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
